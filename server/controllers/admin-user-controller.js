e8aa91f31b01a9a56ebea41e2cfcf39a199554cc
Danieruu <panturasd@gmail.com>
2025-10-10 02:47:23 +0800

    fix deployment2

--- server/controllers/admin-user-controller.js
+++ server/controllers/admin-user-controller.js
@@ -110,8 +110,8 @@ const loginUser = async (req, res) => {
     console.log("Setting refreshToken:", refreshToken);
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     console.log("Cookie set successfully");
@@ -172,8 +172,8 @@ const logout = async (req, res) => {
   try {
     res.clearCookie("refreshToken", {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
     });
     return res.status(200).json({ message: "Logout successfully" });
   } catch (error) {
